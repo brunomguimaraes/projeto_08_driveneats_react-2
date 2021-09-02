345065762f199a48aac0143b5a8dda15dd559158
Bruno <mguimaraesbruno@gmail.com>
2021-09-02 12:03:14 -0300

    Code Review: Brunão

--- src/TopBar.js
+++
@@ -1,8 +0,0 @@
-export default function TopBar() {
-    return (
-        <div class="header">
-            <div class="title">Driven Eats</div>
-            <div class="subtitulo">Sua comida em 6 minutos</div>
-        </div>
-    );
-}
